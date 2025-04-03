36c6a0f624693a2c66732c2a8710998516e04cd0
Jan Korona <jan.korona@gmail.com>
2025-04-03 13:26:45 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Center, Heading, Stack, Text } from "@chakra-ui/react";
+function App() {
+  return (
+    <Center h="100vh">
+      <Stack gap="4" alignItems="center">
+        <Heading size="4xl">Better Workouts</Heading>
+        <Text>Under construction</Text>
+      </Stack>
+    </Center>
+  );
+}
+
+export default App;
